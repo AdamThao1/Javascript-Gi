18b0f1b85c0d93ede6222cd7ddac449c5322205e
AdamThao1 <athao215@gmail.com>
2023-04-23 22:05:18 -0400

    added hard file

--- Easy.js
+++ Easy.js
@@ -1,7 +1,7 @@
 //Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters. Example output: “The name Thomas is longer than John by 2 characters”
-var name1 = "Adam"
-var name2 = "Rhino"
-var e = (name2.length - name1.length)
+var name1 = "Adam"   //variable 1
+var name2 = "Rhino"  //variable 2
+var e = (name2.length - name1.length)   //varible that subtracts name2 to name1
 var t = (name1.length - name2.length)
 
 if (name1.length < name2.length) {
---
+++ hard.js
@@ -0,0 +1,28 @@
+//Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
+
+
+//function that adds two numbers
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(4, 4));
+
+//function thats subtracts two numbers
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+console.log(subtract(6, 2));
+
+//function that multiplies two numbers
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+console.log(multiply(4, 4))
+
+//function that divides two numbers
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+console.log(divide(8, 6))
